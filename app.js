b40f29410cd7d7a58af793e4ac95e903035d17b1
Jacob Wagner <57325512+jakewags01@users.noreply.github.com>
2021-12-03 14:01:00 -0500

    added rest service and changed file locations to adjust

---
+++ app.js
@@ -0,0 +1,68 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(express.static('static'));
+
+app.get('/signin', (req, res) => { // sending the signin html
+  res.sendFile('/static/html/signin.html', {root: __dirname});
+});
+
+app.get('/signup', (req, res) => { // sending the signup html
+  res.sendFile('/static/html/signup.html', {root: __dirname});
+});
+
+app.get('/index', (req, res) => { // sending the index html
+  res.sendFile('/static/html/index.html', {root: __dirname});
+});
+
+app.get('/search', (req, res) => { // sending the search html
+  res.sendFile('/static/html/search.html', {root: __dirname});
+});
+
+app.get('/settings', (req, res) => { // sending the settings html
+  res.sendFile('/static/html/settings.html', {root: __dirname});
+});
+
+app.listen(3001, () => {
+  console.log(`Example app listening at http://localhost:3001`)
+});
+
+
+// view engine setup
+app.set('view', path.join(__dirname, 'view'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
