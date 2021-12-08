c2bf5e0ea2e974404a40c6d09c51a0fab4326dd3
Jacob Wagner <57325512+jakewags01@users.noreply.github.com>
2021-12-08 13:34:25 -0500

    Unpacked .zip

--- app.js
+++ app.js
@@ -3,81 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-/*var soap = require('soap');
-var passport = require('passport');
-var userList = [{username : 'admin', password : 'root'}];
-var localStrat = require('passport-local').Strategy;*/
 
-const url = 'http://localhost:8080/userservice?wsdl';
-const pas = {passHash: 'sadfjkhsafdjh', saltHash: 23482734};
-const args = {username: 'User', email: 'User@gmail.com', password: pas};
-
-var indexRouter = require('./routes');
+// var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var searchRouter = require('./routes/search');
+var settingsRouter = require('./routes/settings');
+var signinRouter = require('./routes/signin');
+var signupRouter = require('./routes/signup');
+var projectsRouter = require('./routes/projects');
 
 var app = express();
 
-app.use(express.static('static'));
-
-//app.use(passport.initialize());
-
-app.get('/signin', (req, res) => { // sending the signin html
-  res.sendFile('/static/html/signin.html', {root: __dirname});
-});
-
-app.get('/signup', (req, res) => { // sending the signup html
-  res.sendFile('/static/html/signup.html', {root: __dirname});
-});
-
-app.get('/index', (req, res) => { // sending the index html
-  res.sendFile('/static/html/index.html', {root: __dirname});
-});
-
-app.get('/search', (req, res) => { // sending the search html
-  res.sendFile('/static/html/search.html', {root: __dirname});
-});
-
-app.get('/settings', (req, res) => { // sending the settings html
-  res.sendFile('/static/html/settings.html', {root: __dirname});
-});
-
-/*passport.use('local', new localStrat(function (username, password, done) {
-  var checkUser = username;
-  var info = userList.find((element) => {
-    if (element.username == checkUser) {
-      return element;
-    }
-  });
-
-  if (info == undefined || info == null) {
-    return done(null, false, {message : 'Incorrect Username'});
-  }
-  if (info.password != password) {
-    return done(null, false, {message : 'Incorrect Password'});
-  }
-  else {
-    return done(null, info);
-  }
-}));
-
-app.post('/signup', (req, res) => {
-  res.sendFile(soap.createClient(url, function(err, client) {
-    client.addUser(args, function(err, result) {
-      console.log(result);
-    })
-  }))
-});*/
-
-//app.post('/signin', );
-
-app.listen(3001, () => {
-  console.log(`Example app listening at http://localhost:3001`)
-});
-
-
 // view engine setup
-app.set('view', path.join(__dirname, 'view'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -85,8 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/search', searchRouter);
+app.use('/settings', settingsRouter);
+app.use('/signin', signinRouter);
+app.use('/signup', signupRouter);
+app.use('/projects', projectsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/projects.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    project1 = {title: 'My First Project', shortDescription: 'quick', lastUpdated: 'never'}
+    project2 = {title: 'My Second Project', shortDescription: 'quick', lastUpdated: 'never'}
+    res.render('projects', { username: 'Jake', userImage: "/images/logo.png", projects: [project1, project2]});
+});
+
+module.exports = router;
