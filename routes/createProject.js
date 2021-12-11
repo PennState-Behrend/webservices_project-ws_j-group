56f42a4fe0622b2871ef13466effc040ec29650c
jdbowers22 <57910863+jdbowers22@users.noreply.github.com>
2021-12-11 17:01:10 -0500

    adding code for handling create project in rest

--- routes/createProject.js
+++ routes/createProject.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('createProject', {});
+    res.render('createProject', {pageTitle: 'settings', username: 'Jake', userImage: "/images/logo2.svg" });
 });
 
 router.post('/', function(req, res, next) {
