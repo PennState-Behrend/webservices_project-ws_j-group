19fdd583b6673e5c686269915d27ed1578373340
jdbowers22 <57910863+jdbowers22@users.noreply.github.com>
2021-12-11 17:57:09 -0500

    adding code for handling delete project in rest

--- routes/deleteProject.js
+++ routes/deleteProject.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('deleteProject', {pageTitle: 'Delete Project', username: 'Jake', userImage: "/images/logo2.svg" });
+    project1 = {title: 'My First Project', shortDescription: 'quick', lastUpdated: 'never'}
+    project2 = {title: 'My Second Project', shortDescription: 'quick', lastUpdated: 'never'}
+    res.render('deleteProject', { pageTitle: 'projects', username: 'Jake', userImage: "/images/logo.png", projects: [project1, project2]});
 });
 
 router.post('/', function(req, res, next) {
